eb937e749cf5c5ca31fb66df60551d07f7b50d2f
Axelh04 <ahernandez53@hawk.iit.edu>
2024-07-24 13:26:15 -0700

    Finished bottom half UI

--- frontend/src/RecordingPageComp/SimilarSounds.jsx
+++ frontend/src/RecordingPageComp/SimilarSounds.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/prop-types */
 
 import { useState } from "react";
+import "./SimilarSounds.css";
 
-function SimilarSounds({ note, playbackDuration }) {
+function SimilarSounds({ playbackDuration }) {
   const duration = Math.floor(playbackDuration);
   const [soundList, setSoundList] = useState([]);
 
@@ -10,7 +11,7 @@ function SimilarSounds({ note, playbackDuration }) {
     try {
       const API_KEY = import.meta.env.VITE_APP_API_KEY;
       const response = await fetch(
-        `https://freesound.org/apiv2/search/text/?token=${API_KEY}&filter=duration:${duration}%20ac_note_midi:${note}&fields=name,previews`
+        `https://freesound.org/apiv2/search/text/?token=${API_KEY}&filter=duration:${duration}&fields=name,previews`
       );
       const soundData = await response.json();
       setSoundList(soundData.results);
@@ -21,17 +22,20 @@ function SimilarSounds({ note, playbackDuration }) {
 
   return (
     <>
-      <button onClick={fetchSounds}>fetch</button>
-
-      {soundList.length > 0 ? (
-        soundList.map((sound, i) => (
-          <div key={i}>
-            <audio controls src={sound.previews["preview-lq-mp3"]}></audio>
-          </div>
-        ))
-      ) : (
-        <div>No boards found.</div>
-      )}
+      <button id="fetch-sounds-button" onClick={fetchSounds}>
+        Get Similar Sounds
+      </button>
+      <div id="similar-sounds-list">
+        {soundList.length > 0 ? (
+          soundList.map((sound, i) => (
+            <div key={i} id="sound-item">
+              <audio controls src={sound.previews["preview-lq-mp3"]}></audio>
+            </div>
+          ))
+        ) : (
+          <div>No boards found.</div>
+        )}
+      </div>
     </>
   );
 }
