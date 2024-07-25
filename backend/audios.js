3419fc1e81fc130f4915b98c8a5c0df73ec55f2c
Axelh04 <ahernandez53@hawk.iit.edu>
2024-07-25 10:06:11 -0700

    Profile Page UI
    Added Profile Page styling and delete buttons

--- backend/audios.js
+++ backend/audios.js
@@ -32,8 +32,21 @@ router.get("/users/:id/audios", async (req, res) => {
     });
     res.status(200).json(audios);
   } catch (error) {
-    console.error("Failed to retrieve cards:", error);
-    res.status(500).json({ error: "Failed to retrieve cards" });
+    console.error("Failed to retrieve audios:", error);
+    res.status(500).json({ error: "Failed to retrieve audios" });
+  }
+});
+
+router.delete("/users/:id/audios/:audioid/delete", async (req, res) => {
+  const { id, audioid } = req.params;
+  try {
+    const deletedAudio = await prisma.recordings.delete({
+      where: { id: parseInt(audioid), userId: parseInt(id) },
+    });
+    res.status(200).json(deletedAudio);
+  } catch (error) {
+    console.error("Failed to delete Audio:", error);
+    res.status(500).json({ error: "Failed to delete Audio" });
   }
 });
 
--- frontend/src/ProfilePageComp/ProfilePage.jsx
+++ frontend/src/ProfilePageComp/ProfilePage.jsx
@@ -1,34 +1,39 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../UserContext.js";
 import { Link } from "react-router-dom";
 import SavedRecordings from "./SavedRecordings.jsx";
 import NavBar from "../NavBarComp/NavBar.jsx";
+import "./ProfilePage.css";
 
 function ProfilePage() {
   const { user, updateUser } = useContext(UserContext);
+  const [recordings, setRecordings] = useState([]);
 
+  console.log(user);
   const handleLogout = () => {
     updateUser(null);
   };
 
   return (
     <div className="main">
-      <header className="header">
-        <div className="user-info">
-          {user ? (
-            <>
-              <div>This is your profile: {user.username}</div>
-              <Link to="/">
-                <button onClick={handleLogout}>Logout</button>
-              </Link>
-            </>
-          ) : (
-            <Link to="/">Back</Link>
-          )}
-        </div>
-      </header>
-
-      <SavedRecordings />
+      <div id="profile-info-title">Profile Information</div>
+      <div id="profile-info-container">
+        {user ? (
+          <div id = "profile-details">
+            <div className="info-text">Your UserID: <span className="user-detail">{user.id}</span></div>
+            <div className="info-text">Your Username: <span className="user-detail">{user.username}</span></div>
+            <div className="info-text">Your Email: <span className="user-detail">{user.email}</span></div>
+            <div className="info-text">Total Recordings: <span className="user-detail">{recordings.length}</span> </div>
+            <Link to="/">
+              <button id = "logout-button" onClick={handleLogout}>Logout</button>
+            </Link>
+          </div>
+        ) : (
+          <Link to="/">Back</Link>
+        )}
+      </div>
+      <div id="profile-recordings-title">All Recordings</div>
+      <SavedRecordings recordings = {recordings} setRecordings ={setRecordings} />
       <NavBar />
     </div>
   );
